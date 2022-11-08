6d1538d0d1e6de0f2585d8e4d17189cc9abeb9ce
Titus Meader <titusmeader@btinternet.com>
2022-11-08 21:28:38 +0000

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Thiscord_Installer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ThiscordBanner = new System.Windows.Forms.PictureBox();
+            this.TermsTextBox = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.InstallButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.LegalTextbox = new System.Windows.Forms.TextBox();
+            this.UninstallButton = new System.Windows.Forms.Button();
+            this.UninstallTextbox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ThiscordBanner)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = global::Thiscord_Installer.Properties.Resources.thiscordbanner;
+            this.pictureBox1.Location = new System.Drawing.Point(286, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ThiscordBanner
+            // 
+            this.ThiscordBanner.BackColor = System.Drawing.Color.Transparent;
+            this.ThiscordBanner.Image = global::Thiscord_Installer.Properties.Resources.thiscordbanner;
+            this.ThiscordBanner.Location = new System.Drawing.Point(17, 12);
+            this.ThiscordBanner.Name = "ThiscordBanner";
+            this.ThiscordBanner.Size = new System.Drawing.Size(750, 79);
+            this.ThiscordBanner.TabIndex = 1;
+            this.ThiscordBanner.TabStop = false;
+            // 
+            // TermsTextBox
+            // 
+            this.TermsTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.TermsTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TermsTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.TermsTextBox.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TermsTextBox.ForeColor = System.Drawing.Color.White;
+            this.TermsTextBox.HideSelection = false;
+            this.TermsTextBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.TermsTextBox.Location = new System.Drawing.Point(17, 122);
+            this.TermsTextBox.Name = "TermsTextBox";
+            this.TermsTextBox.ReadOnly = true;
+            this.TermsTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.TermsTextBox.Size = new System.Drawing.Size(750, 238);
+            this.TermsTextBox.TabIndex = 2;
+            this.TermsTextBox.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(101)))), ((int)(((byte)(242)))));
+            this.panel1.Location = new System.Drawing.Point(0, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 5);
+            this.panel1.TabIndex = 3;
+            // 
+            // InstallButton
+            // 
+            this.InstallButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(101)))), ((int)(((byte)(242)))));
+            this.InstallButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.InstallButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InstallButton.ForeColor = System.Drawing.Color.White;
+            this.InstallButton.Location = new System.Drawing.Point(348, 408);
+            this.InstallButton.Name = "InstallButton";
+            this.InstallButton.Size = new System.Drawing.Size(128, 41);
+            this.InstallButton.TabIndex = 5;
+            this.InstallButton.Text = "Install Thiscord";
+            this.InstallButton.UseVisualStyleBackColor = false;
+            this.InstallButton.Click += new System.EventHandler(this.InstallButton_Click);
+            this.InstallButton.MouseEnter += new System.EventHandler(this.InstallButton_MouseEnter);
+            this.InstallButton.MouseLeave += new System.EventHandler(this.InstallButton_MouseLeave);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(16, 121);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(752, 240);
+            this.panel2.TabIndex = 6;
+            // 
+            // LegalTextbox
+            // 
+            this.LegalTextbox.BackColor = System.Drawing.Color.Black;
+            this.LegalTextbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LegalTextbox.Enabled = false;
+            this.LegalTextbox.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LegalTextbox.ForeColor = System.Drawing.Color.Gainsboro;
+            this.LegalTextbox.HideSelection = false;
+            this.LegalTextbox.Location = new System.Drawing.Point(482, 408);
+            this.LegalTextbox.Multiline = true;
+            this.LegalTextbox.Name = "LegalTextbox";
+            this.LegalTextbox.ReadOnly = true;
+            this.LegalTextbox.Size = new System.Drawing.Size(167, 36);
+            this.LegalTextbox.TabIndex = 7;
+            this.LegalTextbox.Text = "By clicking \"Install Thiscord\" you agree to the terms set out above";
+            // 
+            // UninstallButton
+            // 
+            this.UninstallButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.UninstallButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UninstallButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UninstallButton.ForeColor = System.Drawing.Color.White;
+            this.UninstallButton.Location = new System.Drawing.Point(12, 408);
+            this.UninstallButton.Name = "UninstallButton";
+            this.UninstallButton.Size = new System.Drawing.Size(128, 41);
+            this.UninstallButton.TabIndex = 8;
+            this.UninstallButton.Text = "Uninstall Thiscord";
+            this.UninstallButton.UseVisualStyleBackColor = false;
+            this.UninstallButton.Click += new System.EventHandler(this.UninstallButton_click);
+            this.UninstallButton.MouseEnter += new System.EventHandler(this.UninstallButton_MouseEnter);
+            this.UninstallButton.MouseLeave += new System.EventHandler(this.UninstallButton_MouseLeave);
+            // 
+            // UninstallTextbox
+            // 
+            this.UninstallTextbox.BackColor = System.Drawing.Color.Black;
+            this.UninstallTextbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.UninstallTextbox.Enabled = false;
+            this.UninstallTextbox.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UninstallTextbox.ForeColor = System.Drawing.Color.Gainsboro;
+            this.UninstallTextbox.HideSelection = false;
+            this.UninstallTextbox.Location = new System.Drawing.Point(146, 408);
+            this.UninstallTextbox.Multiline = true;
+            this.UninstallTextbox.Name = "UninstallTextbox";
+            this.UninstallTextbox.ReadOnly = true;
+            this.UninstallTextbox.Size = new System.Drawing.Size(167, 36);
+            this.UninstallTextbox.TabIndex = 9;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.InfoText;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.UninstallTextbox);
+            this.Controls.Add(this.UninstallButton);
+            this.Controls.Add(this.LegalTextbox);
+            this.Controls.Add(this.InstallButton);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.TermsTextBox);
+            this.Controls.Add(this.ThiscordBanner);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Thiscord Installer";
+            this.Load += new System.EventHandler(this.sz_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ThiscordBanner)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox ThiscordBanner;
+        private System.Windows.Forms.RichTextBox TermsTextBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button InstallButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox LegalTextbox;
+        private System.Windows.Forms.Button UninstallButton;
+        private System.Windows.Forms.TextBox UninstallTextbox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thiscord_Installer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InstallButton_MouseEnter(object sender, EventArgs e)
+        {
+            InstallButton.BackColor = Color.FromArgb(49, 57, 140);
+        }
+
+        private void InstallButton_MouseLeave(object sender, EventArgs e)
+        {
+            InstallButton.BackColor = Color.FromArgb(88, 101, 242);
+        }
+
+        private void InstallButton_Click(object sender, EventArgs e)
+        {
+            TermsTextBox.Hide();
+            panel2.Hide();
+            LegalTextbox.Text = "Starting install";
+            string dir;
+            using (var client = new WebClient())
+            {
+                try
+                {
+                    dir = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Thiscord\\";
+                    if (!Directory.Exists(dir))
+                    {
+                        Directory.CreateDirectory(dir);
+                    }
+                    LegalTextbox.Text = "Downloading...";
+                    client.DownloadFile("https://github.com/RJ-Infinity/ThisCord/archive/refs/heads/master.zip", $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Thiscord\\thiscord.zip");
+                }
+                catch (Exception error)
+                {
+                    Clipboard.SetText(error.ToString());
+                    LegalTextbox.Text = $"Error : copied to clipboard";
+                    return;
+                }
+            }
+            dir = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Thiscord\\thiscord.zip";
+            if (!File.Exists(dir))
+            {
+                LegalTextbox.Text = "Error (File not downloaded)";
+                return;
+            }
+            LegalTextbox.Text = "Download Complete";
+            try
+            {
+                dir = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Thiscord\\ThisCord-master";
+                if (Directory.Exists(dir))
+                {
+                    Directory.Delete(dir, true);
+                }
+                ZipFile.ExtractToDirectory($"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Thiscord\\thiscord.zip", $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Thiscord");
+                string sourceFile = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Thiscord\\ThisCord-master\\src\\Update.exe";
+                string destinationFile = $"{ Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Discord\\Update.exe";
+                System.IO.File.Move($"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Discord\\Update.exe", $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Discord\\Update_Discord.exe");
+                File.Copy(sourceFile, destinationFile, true);
+            }
+            catch (Exception error)
+            {
+                Clipboard.SetText(error.ToString());
+                LegalTextbox.Text = $"Error : copied to clipboard";
+                return;
+            }
+            LegalTextbox.Text = "Extracted and Copied Successfully";
+
+            InstallButton.Text = "Installed successfully";
+            InstallButton.Enabled = false;
+        }
+
+        private void sz_Load(object sender, EventArgs e)
+        {
+            string dir = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Thiscord\\ThisCord-master";
+            if (Directory.Exists(dir))
+            {
+                InstallButton.Text = "Reinstall Thiscord";
+            }
+        }
+
+        private void UninstallButton_click(object sender, EventArgs e)
+        {
+            UninstallTextbox.Text = "Uninstalling...";
+            string dir;
+            dir = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Thiscord";
+            if (Directory.Exists(dir))
+            {
+                Directory.Delete(dir, true);
+            }
+            UninstallTextbox.Text = "Removed Folder";
+            dir = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Discord\\Update_Discord.exe";
+            if (File.Exists(dir))
+            {
+                File.Delete($"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Discord\\Update.exe");
+                System.IO.File.Move($"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Discord\\Update_Discord.exe", $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Discord\\Update.exe");
+            }
+            UninstallTextbox.Text = "Reset Discord Appdata Successfully";
+            UninstallButton.Text = "Not Installed";
+            UninstallButton.Enabled = false;
+        }
+
+        private void UninstallButton_MouseEnter(object sender, EventArgs e)
+        {
+            UninstallButton.BackColor = Color.FromArgb(14, 14, 14);
+        }
+
+        private void UninstallButton_MouseLeave(object sender, EventArgs e)
+        {
+            UninstallButton.BackColor = Color.FromArgb(34, 34, 34);
+        }
+    }
+}
