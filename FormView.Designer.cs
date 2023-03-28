213b43f22e637ceabef26547d415b283439ea886
Titus Meader <titusmeader@btinternet.com>
2023-03-28 22:15:54 +0100

    resizing

---
+++ FormView.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Thiscord_Installer
+{
+    partial class FormView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormView));
+            this.ThiscordBanner = new System.Windows.Forms.PictureBox();
+            this.Terms = new System.Windows.Forms.RichTextBox();
+            this.ModifyDiscord = new System.ComponentModel.BackgroundWorker();
+            this.InstallingBar = new System.Windows.Forms.ProgressBar();
+            this.InstallButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ThiscordBanner)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ThiscordBanner
+            // 
+            this.ThiscordBanner.Image = global::Thiscord_Installer.Properties.Resources.thiscordbanner;
+            this.ThiscordBanner.Location = new System.Drawing.Point(-52, -1);
+            this.ThiscordBanner.Name = "ThiscordBanner";
+            this.ThiscordBanner.Size = new System.Drawing.Size(856, 122);
+            this.ThiscordBanner.TabIndex = 0;
+            this.ThiscordBanner.TabStop = false;
+            // 
+            // Terms
+            // 
+            this.Terms.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(51)))), ((int)(((byte)(56)))));
+            this.Terms.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Terms.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Terms.ForeColor = System.Drawing.Color.White;
+            this.Terms.Location = new System.Drawing.Point(3, 127);
+            this.Terms.Name = "Terms";
+            this.Terms.ReadOnly = true;
+            this.Terms.Size = new System.Drawing.Size(785, 254);
+            this.Terms.TabIndex = 1;
+            this.Terms.Text = "";
+            // 
+            // ModifyDiscord
+            // 
+            this.ModifyDiscord.WorkerReportsProgress = true;
+            this.ModifyDiscord.DoWork += new System.ComponentModel.DoWorkEventHandler(this.ModifyDiscord_DoWork);
+            this.ModifyDiscord.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.ModifyDiscord_ProgressChanged);
+            // 
+            // InstallingBar
+            // 
+            this.InstallingBar.Location = new System.Drawing.Point(309, 443);
+            this.InstallingBar.Name = "InstallingBar";
+            this.InstallingBar.Size = new System.Drawing.Size(182, 10);
+            this.InstallingBar.TabIndex = 3;
+            // 
+            // InstallButton
+            // 
+            this.InstallButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(128)))), ((int)(((byte)(70)))));
+            this.InstallButton.FlatAppearance.BorderSize = 0;
+            this.InstallButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(86)))), ((int)(((byte)(43)))));
+            this.InstallButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(99)))), ((int)(((byte)(52)))));
+            this.InstallButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.InstallButton.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InstallButton.ForeColor = System.Drawing.Color.White;
+            this.InstallButton.Location = new System.Drawing.Point(309, 387);
+            this.InstallButton.Name = "InstallButton";
+            this.InstallButton.Size = new System.Drawing.Size(182, 50);
+            this.InstallButton.TabIndex = 6;
+            this.InstallButton.Tag = "Install";
+            this.InstallButton.Text = "Install";
+            this.InstallButton.UseVisualStyleBackColor = false;
+            this.InstallButton.Click += new System.EventHandler(this.InstallButton_Click);
+            // 
+            // FormView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(51)))), ((int)(((byte)(56)))));
+            this.ClientSize = new System.Drawing.Size(800, 459);
+            this.Controls.Add(this.InstallButton);
+            this.Controls.Add(this.InstallingBar);
+            this.Controls.Add(this.Terms);
+            this.Controls.Add(this.ThiscordBanner);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormView";
+            this.Text = "Installer";
+            this.Load += new System.EventHandler(this.FormView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ThiscordBanner)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ThiscordBanner;
+        private System.Windows.Forms.RichTextBox Terms;
+        private System.ComponentModel.BackgroundWorker ModifyDiscord;
+        private System.Windows.Forms.ProgressBar InstallingBar;
+        private System.Windows.Forms.Button InstallButton;
+    }
+}
+
---
+++ FormView.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows.Forms;
+using RJJSON;
+
+namespace Thiscord_Installer
+{
+
+    public partial class FormView : Form
+    {
+        Version LatestVersion = new Version("0.0.0");
+        Color DiscordGreen = Color.FromArgb(36, 128, 70);
+        Color DiscordGreenHover = Color.FromArgb(26, 99, 52);
+        Color DiscordGreenPressed = Color.FromArgb(21, 86, 43);
+        Color DiscordRed = Color.FromArgb(192, 49, 53);
+        Color DiscordRedHover = Color.FromArgb(161, 40, 40);
+        Color DiscordRedPressed = Color.FromArgb(126, 28, 30);
+        string DefaultDiscordPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Discord\\";
+        string DefaultThiscordPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Thiscord";
+        string DefaultUpdateExePath = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Thiscord\\ThisCord-master\\src\\Update.exe";
+        string DefaultDiscordUpdateExePath = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Discord\\Update.exe";
+        private void killDiscordProcesses()
+        {
+            foreach (var process in Process.GetProcessesByName("discord"))
+            {
+                process.Kill();
+            }
+        }
+        private void fileDeleteIfExists(string path)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+        private void dirDeleteIfExists(string path, bool recursive)
+        {
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path, recursive);
+            }
+        }
+        private void fileMoveIfExists(string target, string destination)
+        {
+            if (File.Exists(target) && !File.Exists(destination))
+            {
+                File.Move(target, destination);
+            }
+        }
+        private void fileCopyIfExists(string target, string destination, bool overwrite)
+        {
+            if (File.Exists(target))
+            {
+                if (!File.Exists(destination))
+                {
+                    File.Copy(target, destination, overwrite);
+                }
+                else if (overwrite)
+                {
+                    File.Copy(target, destination, overwrite);
+                }
+            }
+        }
+        private void changeButtonType()
+        {
+            if (InstallButton.Tag.ToString() == "Install")
+            {
+                InstallButton.Text = "Uninstall";
+                InstallButton.BackColor = DiscordRed;
+                InstallButton.FlatAppearance.MouseOverBackColor = DiscordRedHover;
+                InstallButton.FlatAppearance.MouseDownBackColor = DiscordRedPressed;
+                InstallButton.Tag = "Uninstall";
+            }
+            else
+            {
+                InstallButton.Text = $"Install - {LatestVersion}";
+                InstallButton.BackColor = DiscordGreen;
+                InstallButton.FlatAppearance.MouseOverBackColor = DiscordGreenHover;
+                InstallButton.FlatAppearance.MouseDownBackColor = DiscordGreenPressed;
+                InstallButton.Tag = "Install";
+            }
+
+        }
+
+        public FormView()
+        {
+            InitializeComponent();
+        }
+
+        private void Terms_Enter(object sender, EventArgs e)
+        {
+            ActiveControl = null; // Hide i-Beam cursor
+        }
+
+        private void InstallButton_Click(object sender, EventArgs e)
+        {
+            ModifyDiscord.RunWorkerAsync();
+        }
+
+        private void ModifyDiscord_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            int Progress = 0;
+            ModifyDiscord.ReportProgress(0);
+            if (InstallButton.Tag.ToString() == "Install")
+            {
+                killDiscordProcesses();
+                ModifyDiscord.ReportProgress(Progress += 10);
+                if (!Directory.Exists(DefaultDiscordPath))
+                {
+                    DialogResult result = MessageBox.Show($"Discord doesnt seem to be installed\nSee how to fix this if you use a custom install location?",
+                        "Not Installed",
+                        MessageBoxButtons.YesNo
+                    );
+                    if (result == DialogResult.Yes)
+                    {
+                        Process.Start("explorer", "https://github.com/RJ-Infinity/ThisCord/blob/master/customInstall.md");
+                    }
+                    else { return; }
+                }
+
+                dirDeleteIfExists(DefaultThiscordPath, true);
+                ModifyDiscord.ReportProgress(Progress += 10);
+                if (File.Exists($"{DefaultDiscordPath}\\Update_Discord.exe"))
+                {
+                    fileDeleteIfExists($"{DefaultDiscordPath}\\Update.exe");
+                    fileMoveIfExists($"{DefaultDiscordPath}\\Update_Discord.exe", $"{DefaultDiscordPath}\\Update.exe");
+                }
+                ModifyDiscord.ReportProgress(Progress += 10);
+                using (var client = new WebClient())
+                {
+                    try
+                    {
+                        if (!Directory.Exists(DefaultThiscordPath))
+                        {
+                            Directory.CreateDirectory(DefaultThiscordPath);
+                            ModifyDiscord.ReportProgress(Progress += 10);
+                        }
+                        client.DownloadFile("https://github.com/RJ-Infinity/ThisCord/archive/refs/heads/master.zip", $"{DefaultThiscordPath}\\thiscord.zip");
+                        ModifyDiscord.ReportProgress(Progress += 10);
+
+                    }
+                    catch
+                    {
+                        MessageBox.Show($"Failed to download thiscord");
+                        return;
+                    }
+                }
+                ModifyDiscord.ReportProgress(Progress += 10);
+                ZipFile.ExtractToDirectory($"{DefaultThiscordPath}\\thiscord.zip", DefaultThiscordPath);
+                ModifyDiscord.ReportProgress(Progress += 10);
+                fileDeleteIfExists($"{DefaultThiscordPath}\\thiscord.zip");
+                ModifyDiscord.ReportProgress(Progress += 10);
+                fileMoveIfExists(DefaultDiscordUpdateExePath, $"{DefaultDiscordPath}\\Update_Discord.exe");
+                ModifyDiscord.ReportProgress(Progress += 10);
+                fileCopyIfExists(DefaultUpdateExePath, DefaultDiscordUpdateExePath, true);
+                ModifyDiscord.ReportProgress(100);
+                changeButtonType();
+
+            }
+            else if (InstallButton.Tag.ToString() == "Uninstall")
+            {
+                killDiscordProcesses();
+                ModifyDiscord.ReportProgress(Progress += 20);
+
+                dirDeleteIfExists(DefaultThiscordPath, true);
+                ModifyDiscord.ReportProgress(Progress += 20);
+                fileDeleteIfExists(DefaultDiscordUpdateExePath);
+                ModifyDiscord.ReportProgress(Progress += 20);
+                fileMoveIfExists($"{DefaultDiscordPath}\\Update_Discord.exe", DefaultDiscordUpdateExePath);
+                ModifyDiscord.ReportProgress(100);
+                changeButtonType();
+            }
+        }
+
+        private void ModifyDiscord_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            InstallingBar.Value = e.ProgressPercentage;
+        }
+
+        private void FormView_Load(object sender, EventArgs e)
+        {
+            WebClient Client = new WebClient();
+            string Downloaded_Terms = Client.DownloadString("https://raw.githubusercontent.com/RJ-Infinity/ThisCord/master/terms.txt");
+            LatestVersion = new Version(JSON.StringToObject(Client.DownloadString("https://raw.githubusercontent.com/RJ-Infinity/ThisCord/master/version.json"))["version"].StringData);
+            bool updateRequired = JSON.StringToObject(Client.DownloadString("https://raw.githubusercontent.com/RJ-Infinity/ThisCord/master/version.json"))["required"].BoolData;
+            Terms.Text = Downloaded_Terms;
+            if (Directory.Exists(DefaultThiscordPath) && File.Exists($"{DefaultThiscordPath}\\ThisCord-master\\version.json"))
+            {
+                changeButtonType();
+                Version InstalledVersion = new Version(JSON.StringToObject(File.ReadAllText($"{DefaultThiscordPath}\\ThisCord-master\\version.json"))["version"].StringData);
+                if (LatestVersion > InstalledVersion)
+                {
+                    InstallButton.Text = $"Update - {LatestVersion}";
+                }
+            }
+            else
+            {
+                InstallButton.Text = $"Install - {LatestVersion}";
+            }
+        }
+    }
+}
